1a978816bee7e380fc39466d9aa430ed9289cdd7
George Shammas <george@shamm.as>
2022-11-21 22:24:54 -0500

    autoformat

--- webrotater/src/routes/+page.ts
+++ webrotater/src/routes/+page.ts
@@ -1,24 +1,20 @@
 import type { PageLoad } from './$types';
- 
 interface urlList {
-    url: string
-    wait?: number
+	url: string;
+	wait?: number;
 }
 
 export const load: PageLoad = async ({ url, fetch, params }) => {
-  let sourceURL = url.searchParams.get("url") || ""
-  let switchTime = parseInt(url.searchParams.get("delay") || "30") * 1000
+	let sourceURL = url.searchParams.get('url') || '';
+	let switchTime = parseInt(url.searchParams.get('delay') || '30') * 1000;
 
-  let urls: urlList[] 
-  try {
-  const res = await fetch(sourceURL);
-  urls = await res.json();
-  } catch (err) {
-    urls = [
-      { "url":  "/info" },
-    ]
- }
+	let urls: urlList[];
+	try {
+		const res = await fetch(sourceURL);
+		urls = await res.json();
+	} catch (err) {
+		urls = [{ url: '/info' }];
+	}
 
-
-  return { urls, switchTime}
-}
+	return { urls, switchTime };
+};
